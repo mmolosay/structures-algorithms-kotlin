5d07aa98da355071680c6792ce4f655de51f826c
ordo <ordogod00@gmail.com>
2019-10-28 19:38:53 +0300

    Quicksort implementation

--- src/main/kotlin/algoritm/Sort.kt
+++ src/main/kotlin/algoritm/Sort.kt
@@ -19,6 +19,30 @@ object Sort {
         merge(array, l, r)
     }
 
+    fun quickSort(array: Array<Int>, start: Int = 0, end: Int = array.lastIndex) {
+        if (array.size < 2) return
+        if (start >= end) return
+
+        val pivotEl = array[start + (end - start) / 2]
+        var i = start
+        var j = end
+
+        while (i <= j) {
+            while (array[i] < pivotEl) i++
+            while (array[j] > pivotEl) j--
+
+            if (i <= j) {
+                swap(array, i, j)
+                i++; j--
+            }
+        }
+
+        if (start < j)
+            quickSort(array, start, j)
+        if (end > i)
+            quickSort(array, i, end)
+    }
+
     private fun merge(into: Array<Int>, l: Array<Int>, r: Array<Int>) {
         var i = 0
         var j = 0
@@ -37,5 +61,11 @@ object Sort {
 
         while (k < r.size)
             into[i++] = r[k++]
+    }
+
+    private fun swap(array: Array<Int>, i: Int, j: Int) {
+        val jElem = array[j]
+        array[j] = array[i]
+        array[i] = jElem
     }
 }
