dbc7d79f2ef543c7846e05f5561afedbfed2b635
ordo <ordogod00@gmail.com>
2019-11-18 19:03:45 +0300

    Set implementation

---
+++ src/main/kotlin/structure/Set.kt
@@ -0,0 +1,38 @@
+package structure
+
+/**
+ * Created by ordogod on 16.11.2019.
+ **/
+
+class Set<E>(val array: ArrayList<E>) {
+
+    val size: Int
+        get() = array.size
+
+    init {
+        for (i in array.indices)
+            for (j in i+1 until array.size)
+                if (array[i] == array[j])
+                    array.removeAt(j)
+    }
+
+    fun add(element: E): Boolean {
+        if (contains(element)) return false
+        array.add(element)
+        return true
+    }
+
+    fun remove(element: E): Boolean {
+        if (contains(element)) {
+            array.remove(element)
+            return true
+        }
+        return false
+    }
+
+    fun contains(element: E): Boolean {
+        for (item in array)
+            if (item == element) return true
+        return false
+    }
+}
